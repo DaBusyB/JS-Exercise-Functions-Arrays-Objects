77825bf6ff035d77cf8b6cd46e2d59a24478d74c
DaBusyB <brandiapetsi@gmail.com>
2020-05-13 23:17:03 -0400

    Work on last stretch

--- index.js
+++ index.js
@@ -365,7 +365,14 @@ function getGermanCars(inventory) {
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(odometer) {
+  const odometerObj = {
+    odometerNum: odometer,
+    drive: function(distance) {
+      this.odometerNum += distance
+      return this.odometerNum 
+    }
+  }
+
 }
 
